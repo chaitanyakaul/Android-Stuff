5a7259d72f36f149548b071ea3f8f8cc43962a52
chaitanyakaul <kaul.c@husky.neu.edu>
2017-10-01 18:30:08 -0400

    Initial commit of Android wear related stuff

---
+++ AndroidWear/app/src/main/java/com/example/chaitanyakaul/androidwear/MainActivity.java
@@ -0,0 +1,146 @@
+package com.example.chaitanyakaul.androidwear;
+
+import android.content.Context;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.view.BoxInsetLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends WearableActivity {
+
+    private TextView textView;
+    private int counter = 0;
+    private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
+            new SimpleDateFormat("HH:mm", Locale.US);
+
+    private BoxInsetLayout mContainerView;
+    private TextView mTextView;
+    private TextView mClockView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setAmbientEnabled();
+
+        mContainerView = (BoxInsetLayout) findViewById(R.id.container);
+        mTextView = (TextView) findViewById(R.id.text);
+        //mClockView = (TextView) findViewById(R.id.clock);
+        String abc = readFromFile(this);
+        Log.d("Read is", abc);
+        counter = Integer.parseInt(abc);
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setText(abc);
+
+
+
+
+
+    }
+
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("config.txt");
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((receiveString = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+
+    }
+
+
+    public void beginCountdown(View v)
+    {
+        //Tag t = new Tag();
+        Log.d("geko","Hello World");
+        textView = (TextView)findViewById(R.id.textView);
+        counter++;
+
+        textView.setText(String.valueOf(counter));
+        writeToFile(String.valueOf(counter), this);
+
+
+    }
+
+    private void writeToFile(String data,Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+            Log.d("FILE_LOG", "log file writted");
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+
+    }
+
+    @Override
+    public void onEnterAmbient(Bundle ambientDetails) {
+        super.onEnterAmbient(ambientDetails);
+        updateDisplay();
+    }
+
+    @Override
+    public void onUpdateAmbient() {
+        super.onUpdateAmbient();
+        updateDisplay();
+    }
+
+    @Override
+    public void onExitAmbient() {
+        updateDisplay();
+        super.onExitAmbient();
+    }
+
+
+
+
+    private void updateDisplay() {
+        if (isAmbient()) {
+            //mContainerView.setBackgroundColor(getResources().getColor(android.R.color.black));
+            //mTextView.setTextColor(getResources().getColor(android.R.color.white));
+           // mClockView.setVisibility(View.VISIBLE);
+
+            //mClockView.setText(AMBIENT_DATE_FORMAT.format(new Date()));
+        } else {
+           // mContainerView.setBackground(null);
+            //mTextView.setTextColor(getResources().getColor(android.R.color.black));
+            //mClockView.setVisibility(View.GONE);
+        }
+    }
+}
